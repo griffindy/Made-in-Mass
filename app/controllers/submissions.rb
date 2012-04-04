c4fb35d28922edf2c4e991e9e9d86eed5316b7cd
Dylan Griffin <griffindy@gmail.com>
2012-04-03 21:07:13 -0400

    adding in submissions

--- app/controllers/submissions.rb
+++ app/controllers/submissions.rb
@@ -1,29 +1,46 @@
 MadeInMass.controllers :submissions do
-  # get :index, :map => "/foo/bar" do
-  #   session[:foo] = "bar"
-  #   render 'index'
-  # end
-
-  # get :sample, :map => "/sample/url", :provides => [:any, :js] do
-  #   case content_type
-  #     when :js then ...
-  #     else ...
-  # end
-
-  # get :foo, :with => :id do
-  #   "Maps to url '/foo/#{params[:id]}'"
-  # end
-
-  # get "/example" do
-  #   "Hello world!"
-  # end
+  get :index do
+    @submissions = Submission.all()
+    render 'submissions/index'
+  end
 
   get :new, :map => '/submit' do
-    render 'submissions/new', :layout => false
+    render 'submissions/new'
   end
 
   post :create do
+    @submission = Submission.new(params[:submission])
+    if @submission.save
+      flash[:notice] = 'Submission was successfully created.'
+      redirect url(:submissions, :edit, :id => @submission.id)
+    else
+      render 'submissions/new'
+    end
+  end
 
+  get :edit, :with => :id do
+    @submission = Submission.find(params[:id])
+    render 'submissions/edit'
+  end
+
+  put :update, :with => :id do
+    @submission = Submission.find(params[:id])
+    if @submission.update_attributes(params[:submission])
+      flash[:notice] = 'Submission was successfully updated.'
+      redirect url(:submissions, :edit, :id => @submission.id)
+    else
+      render 'submissions/edit'
+    end
+  end
+
+  delete :destroy, :with => :id do
+    submission = Submission.find(params[:id])
+    if submission.destroy
+      flash[:notice] = 'Submission was successfully destroyed.'
+    else
+      flash[:error] = 'Unable to destroy Submission!'
+    end
+    redirect url(:submissions, :index)
   end
   
 end
